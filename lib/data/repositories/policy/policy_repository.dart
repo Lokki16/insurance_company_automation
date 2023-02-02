3b46d1e9f2e5a2d6d8621b3ebd205300433a71f7
Yuki <52448072+Lokki16@users.noreply.github.com>
2023-02-02 09:02:50 +0600

    update

--- lib/data/repositories/policy/policy_repository.dart
+++ lib/data/repositories/policy/policy_repository.dart
@@ -1,17 +1,17 @@
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import 'package:insurance_company_automation/presentation/template/template.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:insurance_company_automation/presentation/template/template.dart';
 
-// class PolicyRepository {
-//   final url = 'https://'
+class PolicyRepository {
+  final url =
+      'https://github.com/Lokki16/insurance_company_automation/blob/main/test.json';
 
-//   Future<PolicyModel> getPolicy() async {
-//     try {
-//       final response = await ;
-//       return PolicyModel.fromJson(json.decode(respose.body));
-//     }
-//     catch (e) {
-//       throw Exception(e.toString());
-//     }
-//   }
-// }
+  Future<PolicyModel> getPolicy() async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      return PolicyModel.fromJson(json.decode(response.body));
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
